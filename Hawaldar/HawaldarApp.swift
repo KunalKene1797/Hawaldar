9aa549841f996ce50fc074cb639ae7aacfd20b49
Kunal Kene <kunalkene1797@gmail.com>
2024-04-01 14:30:59 -0500

    Initial Commit

---
+++ Hawaldar/HawaldarApp.swift
@@ -0,0 +1,17 @@
+//
+//  HawaldarApp.swift
+//  Hawaldar
+//
+//  Created by Kunal Kene on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HawaldarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
